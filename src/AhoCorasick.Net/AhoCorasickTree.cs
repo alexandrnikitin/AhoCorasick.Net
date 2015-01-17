e3232b5d8a68b80edb5404886e9b9cf8a6bb55c3
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2015-01-17 15:08:55 +0200

    Moved everything to one file

--- src/AhoCorasick.Net/AhoCorasickTree.cs
+++ src/AhoCorasick.Net/AhoCorasickTree.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AhoCorasick.Net
 {
@@ -103,6 +104,100 @@ namespace AhoCorasick.Net
                 }
 
                 currentNode.Failure = failure;
+            }
+        }
+
+        private class AhoCorasickTreeNode
+        {
+            public readonly AhoCorasickTreeNode Parent;
+            public AhoCorasickTreeNode Failure;
+            public bool IsFinished;
+            public readonly char Key;
+
+            private int[] _buckets;
+            private int _count;
+            private Entry[] _entries;
+
+            internal AhoCorasickTreeNode()
+                : this(null, ' ')
+            {
+            }
+
+            private AhoCorasickTreeNode(AhoCorasickTreeNode parent, char key)
+            {
+                Key = key;
+                Parent = parent;
+
+                _buckets = new int[0];
+                _entries = new Entry[0];
+            }
+
+            public AhoCorasickTreeNode[] Nodes
+            {
+                get { return _entries.Select(x => x.Value).ToArray(); }
+            }
+
+            public AhoCorasickTreeNode AddNode(char key)
+            {
+                var node = new AhoCorasickTreeNode(this, key);
+
+                var newSize = _count + 1;
+                Resize(newSize);
+
+                var targetBucket = key % newSize;
+                _entries[_count].Key = key;
+                _entries[_count].Value = node;
+                _entries[_count].Next = _buckets[targetBucket];
+                _buckets[targetBucket] = _count;
+                _count++;
+
+                return node;
+            }
+
+            public AhoCorasickTreeNode GetNode(char key)
+            {
+                if (_count == 0) return null;
+
+                var bucketIndex = key % _count;
+                for (var i = _buckets[bucketIndex]; i >= 0; i = _entries[i].Next)
+                {
+                    if (_entries[i].Key == key)
+                    {
+                        return _entries[i].Value;
+                    }
+                }
+
+                return null;
+            }
+
+            private void Resize(int newSize)
+            {
+                var newBuckets = new int[newSize];
+                for (var i = 0; i < newSize; i++)
+                {
+                    newBuckets[i] = -1;
+                }
+
+                var newEntries = new Entry[newSize];
+                Array.Copy(_entries, 0, newEntries, 0, _entries.Length);
+
+                // rebalancing buckets
+                for (var i = 0; i < newSize; i++)
+                {
+                    var bucket = newEntries[i].Key % newSize;
+                    newEntries[i].Next = newBuckets[bucket];
+                    newBuckets[bucket] = i;
+                }
+
+                _buckets = newBuckets;
+                _entries = newEntries;
+            }
+
+            private struct Entry
+            {
+                public char Key;
+                public int Next;
+                public AhoCorasickTreeNode Value;
             }
         }
     }
--- src/AhoCorasick.Net/Entry.cs
+++
@@ -1,9 +0,0 @@
-namespace AhoCorasick.Net
-{
-    internal struct Entry
-    {
-        public char Key;
-        public int Next;
-        public AhoCorasickTreeNode Value;
-    }
-}
